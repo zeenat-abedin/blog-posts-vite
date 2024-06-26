8d614229314265432d68ab7c2c6ba2252659f867
zeenat <zeenu.23@gmail.com>
2024-06-26 21:10:52 +0530

    update image thumbnails for each post

--- src/App.jsx
+++ src/App.jsx
@@ -23,50 +23,49 @@ function App() {
     {
      title: "Random Title",
      excerpt: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-     imageThumbnail: "https://placeimg.com/200/150/nature",
+     imageThumbnail: "https://media.istockphoto.com/id/922745190/photo/blogging-blog-concepts-ideas-with-worktable.jpg?s=1024x1024&w=is&k=20&c=47-y8UPbITvHDInivDBKJJiTMzO6Ds78N2UHQUjQZqk=",
      timestamp: "2024-02-10T09:10:00.000Z"
     },
     {
      title: "Data Science",
      excerpt: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-     imageThumbnail: "https://placeimg.com/200/150/animals",
+     imageThumbnail: "https://images.unsplash.com/photo-1523634921619-37ce98c1877f?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
      timestamp: "2024-02-15T18:05:00.000Z"
     },
-    // Additional items (placeholders)
     {
      title: "Placeholder 6",
      excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-     imageThumbnail: "https://placekitten.com/202/150",
+     imageThumbnail: "https://images.unsplash.com/photo-1534946405874-8915c3f62beb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
      timestamp: "2024-02-22T14:40:00.000Z"
     },
     {
      title: "Placeholder 7",
      excerpt: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-     imageThumbnail: "https://placeimg.com/202/150/tech",
+     imageThumbnail: "https://images.unsplash.com/photo-1706264337427-fbd7405c3483?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
      timestamp: "2024-02-25T21:15:00.000Z"
     },
     {
      title: "Placeholder 8",
      excerpt: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-     imageThumbnail: "https://placekitten.com/203/150",
+     imageThumbnail: "https://images.unsplash.com/photo-1573675580577-58973eafd7ab?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
      timestamp: "2024-02-12T11:50:00.000Z"
     },
     {
      title: "Placeholder 9",
      excerpt: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-     imageThumbnail: "https://placeimg.com/203/150/nature",
+     imageThumbnail: "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
      timestamp: "2024-02-14T16:25:00.000Z"
     },
     {
      title: "Placeholder 10",
      excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-     imageThumbnail: "https://placekitten.com/204/150",
+     imageThumbnail: "https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
      timestamp: "2024-02-27T10:00:00.000Z"
     },
     {
      title: "Placeholder 11",
      excerpt: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-     imageThumbnail: "https://placeimg.com/204/150/animals",
+     imageThumbnail: "https://images.unsplash.com/photo-1455849318743-b2233052fcff?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
      timestamp: "2024-02-16T19:35:00.000Z"
     },
   ]
